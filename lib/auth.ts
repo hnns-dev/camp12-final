09621ef9cbbadee7f6eabb9958bb1574fd70f865
Dan McAtee <danielmcatee@me.com>
2024-08-20 14:34:56 +0200

    add additional auth stuff

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,10 @@
-import { Lucia } from "lucia";
+import { Lucia, Session, User } from "lucia";
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 import { PrismaClient } from "@prisma/client";
+import { User as DBUser } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { cache } from "react";
 
 const client = new PrismaClient();
 
@@ -16,11 +20,70 @@ export const lucia = new Lucia(adapter, {
       secure: process.env.NODE_ENV === "production",
     },
   },
+  getUserAttributes: (attributes) => {
+    return {
+      id: attributes.id,
+      email: attributes.email,
+      name: attributes.name,
+    };
+  },
 });
 
 // IMPORTANT!
 declare module "lucia" {
   interface Register {
     Lucia: typeof lucia;
+    DatabaseUserAttributes: DBUser;
+  }
+}
+
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+    // next.js throws when you attempt to set cookie when rendering page
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes
+        );
+      }
+    } catch {}
+    return result;
+  }
+);
+
+export async function protectPage() {
+  const { user } = await validateRequest();
+  if (!user) {
+    return redirect("/login");
+  }
+  return user;
+}
+
+export async function assertNotAuthenticated() {
+  const { user } = await validateRequest();
+  if (user) {
+    return redirect("/");
   }
 }
