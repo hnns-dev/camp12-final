fa101942b766a1aa07d9c7479af48669d4ebad3c
AkimZmerli <akimdotzmerli@outlook.com>
2024-08-20 15:30:42 +0200

    implement shacnUI Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import { InteractionBar } from "@/components/InteractionBar";
+import Navbar from "@/components/Navbar";
+import { Form } from "@/components/ui/form";
+import { ProfileForm } from "@/components/profile-form";
 
 export default function Home() {
-  return <main className="min-h-screen">
-    <InteractionBar />
-  </main>;
+  return (
+    <main className="min-h-screen">
+      <InteractionBar />
+      <ProfileForm />
+      <Navbar />
+    </main>
+  );
 }
