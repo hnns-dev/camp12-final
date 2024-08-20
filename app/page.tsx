cecff8e187afadde221f4bbeeeb96ba759aab2f3
Nikita Nakropin <nikita@nakropin.com>
2024-08-20 18:42:40 +0200

    added screen-without-bar in the tailwind config to have a precalculated fixed frame size for the map (100vw - 88px). moved the assets from leaflet into the public folder to fix a compability bug with next.js. map is running basic setup example of leaflet

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { InteractionBar } from "@/components/InteractionBar";
-import Map from "@/components/map";
+import dynamic from "next/dynamic";
+const Map = dynamic(() => import("@/components/Map"), { ssr: false });
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="w-screen h-screen">
       <Map />
+      {/* <div className="bg-red-300 w-screen h-screen-without-bar"></div> */}
       <InteractionBar />
     </main>
   );
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,24 +1,29 @@
 "use client";
+import "leaflet/dist/leaflet.css";
 
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+
+const position: [number, number] = [51.505, -0.09];
 
 export default function Map() {
-  return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={false}
-      className="w-screen h-3/4"
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
+  if (typeof window !== "undefined") {
+    return (
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        className="w-screen h-screen-without-bar"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    );
+  }
 }
