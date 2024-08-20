79fa7d191e2a5316177655c50f576466e8f3eb65
Nikita Nakropin <nikita@nakropin.com>
2024-08-20 14:50:50 +0200

    added react leaflet

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
 import { InteractionBar } from "@/components/InteractionBar";
+import Map from "@/components/map";
 
 export default function Home() {
-  return <main className="min-h-screen">
-    <InteractionBar />
-  </main>;
+  return (
+    <main className="min-h-screen">
+      <Map />
+      <InteractionBar />
+    </main>
+  );
 }
---
+++ components/Map.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+export default function Map() {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="w-screen h-3/4"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
