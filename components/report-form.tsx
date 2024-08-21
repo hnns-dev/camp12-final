c2882efa6391ae6e8081624d83b0f07ac6c27969
Fred <frederik.koenig@posteo.de>
2024-08-21 17:04:11 +0200

    added date&time picker, select menu

--- components/report-form.tsx
+++ components/report-form.tsx
@@ -1,10 +1,49 @@
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { DateTimePicker } from "./date-time-picker";
 
 export default function ReportForm() {
   return (
-    <div className="flex flex-col items-center">
-      <p>Whats wrong with this venue?</p>
-      <Input placeholder="Type in the issue" className="m-3"></Input>
-    </div>
+    <>
+      <div className="flex flex-col m-3 text-sm gap-2">
+        <p>What is wrong with this venue?</p>
+        <Select>
+          <SelectTrigger>
+            <SelectValue placeholder="Choose an issue" />
+          </SelectTrigger>
+          <SelectContent className="bg-white p-3">
+            <SelectGroup>
+              <SelectItem value="net">no net/net damaged</SelectItem>
+              <SelectItem value="surface">Surface severly damaged</SelectItem>
+              <SelectItem value="uneven">surface uneven</SelectItem>
+              <SelectItem value="edges">chipped edges</SelectItem>
+              <SelectItem value="safety">
+                safety hazards - please specify below
+              </SelectItem>
+              <SelectItem value="other">
+                other - please specify below
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="flex flex-col m-3 text-sm gap-2">
+        {/* Or shouldn't it be just the date&time of the report automatically? */}
+        <p>When did you see the problem?</p>
+        <DateTimePicker />
+      </div>
+      <div className="flex flex-col m-3 text-sm gap-2">
+        <p>Do you have more details to report?</p>
+        <Input placeholder="Type in details"></Input>
+      </div>
+    </>
   );
 }
