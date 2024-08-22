d1652e7b3e5e7d08ec07f25cbc8e449be598c069
ShaQay3Q <shaghay3gh@gmail.com>
2024-08-22 11:08:02 +0200

    QR-Code is being generated for the hard coded url

--- components/QRCodeGenerator.tsx
+++ components/QRCodeGenerator.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import QRCode from "react-qr-code";
 import { useState } from "react";
@@ -7,28 +7,25 @@ export default function QRCodeGenerator() {
 	const [value, setValue] = useState<string>("");
 	const [displayCode, setDisplayCode] = useState<boolean>(false);
 
-	const generateQRCodeHandler = () => {
+	const userID = "123456";
+	const userURL = `http://localhost:3000/profile/${userID}`;
+
+	useEffect(() => {
+		setValue(userURL);
+	}, [userURL]);
+
+	function generateQRCodeHandler() {
 		if (!value) {
 			return;
 		} else {
 			setDisplayCode(true);
 		}
-	};
+	}
 
 	return (
 		<div className='h-full w-full flex flex-col gap-5 items-center justify-center'>
-			<h1 className='text-3xl xl:text-xl font-semibold'>
-				QR-Code Generator 🔥
-			</h1>
-			<input
-				className='border-solid border-2 rounded border-zinc-500 w-1/2 ls:w-1/3 max-h-9 min-h-5  text-lg'
-				type='text'
-				placeholder='Type a URL...'
-				value={value}
-				onChange={(e) => {
-					setValue(e.target.value);
-				}}
-			/>
+			<h1 className='text-xl font-semibold'>Add to your friends list...</h1>
+
 			<button
 				className='bg-zinc-800 px-4 py-2 text-slate-50 rounded-md w-1/2 ls:w-1/3'
 				onClick={generateQRCodeHandler}
