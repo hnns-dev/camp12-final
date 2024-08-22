24f2f3ad7e755c4331068bc97bc0d8843da056ee
ShaQay3Q <shaghay3gh@gmail.com>
2024-08-22 16:08:08 +0200

    QR Code can now be generated on qr-add-friend page

--- components/QRCodeGenerator.tsx
+++ components/QRCodeGenerator.tsx
@@ -5,41 +5,24 @@ import { useState } from "react";
 
 export default function QRCodeGenerator() {
 	const [value, setValue] = useState<string>("");
-	const [displayCode, setDisplayCode] = useState<boolean>(false);
 
-	const userID = "qr-add-friend";
-	const userURL = `http://localhost:3000/${userID}`;
+	const userID = "123456";
+	const userURL = `http://localhost:3000/profile/${userID}`;
 
 	useEffect(() => {
 		setValue(userURL);
 	}, [userURL]);
 
-	function generateQRCodeHandler() {
-		if (!value) {
-			return;
-		} else {
-			setDisplayCode(true);
-		}
-	}
-
 	return (
 		<div className='h-full w-full flex flex-col gap-5 items-center justify-center my-6'>
-			<h1 className='text-lg font-medium'>Add to your friends list...</h1>
-
-			<button
-				className='bg-zinc-800 px-4 py-2 text-slate-50 rounded-md w-1/2'
-				onClick={generateQRCodeHandler}
-			>
-				Generate QR-Code
-			</button>
-			{displayCode && (
+			{
 				<div>
 					<QRCode
 						size={200}
 						value={value}
 					/>
 				</div>
-			)}
+			}
 		</div>
 	);
 }
