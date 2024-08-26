7e3709e560d51a68d7bc8ec68ddfd6bd5af817d0
Nikita Nakropin <nikita@nakropin.com>
2024-08-26 11:44:03 +0200

    implemented functionality to move the user to the nearest venue on the map with an animated zoom

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { DrawerHompage } from "@/components/DrawerHompage";
 import Navbar from "../components/Navbar";
-import { Map } from "@/components/Map";
+import Map from "@/components/Map";
 
 export default function Home() {
   return (
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,32 +1,75 @@
 "use client";
-import { LatLngExpression } from "leaflet";
+import { LatLng, LatLngExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
-import data from "@/lib/output_data.json";
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+  Popup,
+  useMapEvents,
+  useMap,
+} from "react-leaflet";
+import data from "@/lib/filtered_output_data.json";
 
-//////////////////////////
-// NOT DSVGO COMPLIANT //
-/////////////////////////
-export function Map() {
-  const [location, setLocation] = useState<LatLngExpression | null>(null);
+export default function Map() {
+  // location of venue or view, etc..
+  const [location, setlocation] = useState<LatLngExpression | null>(null);
+  // location of user atm
+  const [userPosition, setUserPosition] = useState<LatLngExpression | null>(
+    null
+  );
   const [loading, setLoading] = useState<boolean>(true);
 
+  // Helper function to convert "LatLng(50,50) to [50, 50]. Maybe deserves its own issue of handling types"
+  function latLngToArray(latLng: any): [number, number] {
+    return [latLng.lat, latLng.lng];
+  }
+
+  // function to locate nearest venue, takes userLocation as parameter
+  function getNearestVenue(userLocation: LatLngExpression): LatLngExpression {
+    // More robust in declaring userLocation this way
+    const userLoc = latLngToArray(userLocation);
+    let result: LatLngExpression = userLocation;
+    let minDistance = 9999;
+    //Go trough data
+    for (let key in data) {
+      const venueLoc: [number, number] = data[key].geolocation as [
+        number,
+        number
+      ];
+      // calculate distance with absolute values
+      let distance =
+        Math.sqrt(Math.pow(userLoc[0] - venueLoc[0], 2)) +
+        Math.sqrt(Math.pow(userLoc[1] - venueLoc[1], 2));
+      // If the distance is smaller then the current minDistance...
+      if (distance < minDistance) {
+        // then make it the new minDistance
+        minDistance = distance;
+        // and set the result to the location of the venue
+        result = venueLoc;
+      }
+    }
+    return result;
+  }
+
+  // Ask if we can use geolocation of device
   useEffect(() => {
     if ("geolocation" in navigator) {
-      // Get your position
+      // if we have allowance, we can set the current position
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          // set the coordinates to your position
           const { latitude, longitude } = position.coords;
-          setLocation([latitude, longitude]);
+          const userPos: LatLngExpression = [latitude, longitude];
+          setUserPosition(userPos);
+          setlocation(getNearestVenue(userPos));
           setLoading(false);
-          console.log(`Latitude: ${latitude}, longitude: ${longitude}`);
         },
+        //If permission denied, go to the custom location
         (error) => {
-          // when initially loaded or if there is an error, go to the default location
           console.error("Error getting User location", error);
-          setLocation([51.3397, 12.3731]);
+          setUserPosition([51.3397, 12.3731]);
+          setlocation([51.3397, 12.3731]);
           setLoading(false);
         }
       );
@@ -36,17 +79,35 @@ export function Map() {
     }
   }, []);
 
-  if (loading) {
-    console.log("Waiting for your location");
-    setLoading(false);
+  function MapEvents() {
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        const newUserPos = e.latlng;
+        setUserPosition(newUserPos);
+        const newlocation = getNearestVenue(newUserPos);
+        setlocation(newlocation);
+        map.flyTo(newlocation, map.getZoom());
+      },
+    });
+    return null;
   }
 
-  // typeof window is needed to avoid webpack ReferenceError
-  if (typeof window !== "undefined" && location !== null && loading === false) {
+  if (loading) {
+    return (
+      <div className="w-screen h-screen-without-bar bg-zinc-300 text-center p-20">
+        Loading
+      </div>
+    );
+  }
+
+  if (typeof window !== "undefined" && userPosition !== null) {
     return (
       <MapContainer
-        center={location}
-        zoom={13}
+        center={userPosition}
+        zoom={18}
         scrollWheelZoom={false}
         className="w-screen h-screen-without-bar"
       >
@@ -63,6 +124,17 @@ export function Map() {
             ]}
           />
         ))}
+        {userPosition && (
+          <Marker position={userPosition}>
+            <Popup>You are here</Popup>
+          </Marker>
+        )}
+        {location && (
+          <Marker position={location}>
+            <Popup>Nearest Venue</Popup>
+          </Marker>
+        )}
+        <MapEvents />
       </MapContainer>
     );
   } else {
