98afe8be0bc0a2c4f1d2ee2f29a62db24bab34f3
Hydrateurself <bensn@posteo.de>
2024-08-27 11:33:09 +0200

    update Settings on submit

---
+++ actions/settings.ts
@@ -0,0 +1,23 @@
+"use server";
+import { prisma } from "@/lib/db";
+
+interface SettingsProps {
+  friendsVisible: string;
+  profileVisible: string;
+  userId: string;
+}
+
+// Function to handle the form submission
+export const updateSettings = async ({
+  friendsVisible,
+  profileVisible,
+  userId,
+}: SettingsProps) => {
+  await prisma.settings.update({
+    where: { userId: userId },
+    data: {
+      friendsVisibility: friendsVisible,
+      profileVisibility: profileVisible,
+    },
+  });
+};
