130fe6d677dc0e5221063969dc2336c3453a3e23
ShaQay3Q <shaghay3gh@gmail.com>
2024-08-28 16:26:05 +0200

    add redirect functionality to add friend and remove friend

--- app/api/connect/route.ts
+++ app/api/connect/route.ts
@@ -1,18 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { addFriend } from '@/lib/utils/add-friend'
-import { redirect } from 'next/navigation';
+import { addFriend } from '@/lib/utils/add-friend';
 
 
 export async function GET(req: NextRequest) {
   const userIdOne = req.nextUrl.searchParams.get("user-one") as string
   const userIdTwo = req.nextUrl.searchParams.get("user-two") as string
+
   try {
     // Call the addFriend function to add a friend
     await addFriend(userIdOne, userIdTwo);
         
-    // redirect("/profile")
-    return NextResponse.json({ message: 'Added successfully to friends list' }, { status: 200 });
-  } catch (error) {
+    // Re-direct to another page after adding a friend
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: "/profile",
+      },
+    });  } catch (error) {
     return NextResponse.json({ error: 'Failed to add into friends list' }, { status: 500 });
   }
 }
--- app/api/unfriend/route.ts
+++ app/api/unfriend/route.ts
@@ -1,15 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { removeFriend } from '@/lib/utils/remove-friend';
 
-
 export async function GET(req: NextRequest) {
   const userIdOne = req.nextUrl.searchParams.get("user-one") as string
   const userIdTwo = req.nextUrl.searchParams.get("user-two") as string
+
   try {
     // Call the removeFriend function to remove a friend
     await removeFriend(userIdOne, userIdTwo);
 
-    return NextResponse.json({ message: 'Removed successfully from friends list' }, { status: 200 });
+    // Re-direct to another page after removing a friend
+
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: "/protected",
+      },
+    });
   } catch (error) {
     return NextResponse.json({ error: 'Failed to remove the user from friends list' }, { status: 500 });
   }
