1ccdf9c926bb19cdcc6c5245f562e785cdc7e6b4
Dan McAtee <danielmcatee@me.com>
2024-08-31 09:43:16 +0200

    unify visibility enum

--- actions/settings.ts
+++ actions/settings.ts
@@ -1,11 +1,11 @@
 "use server";
 import { prisma } from "@/lib/db";
 import { Tag } from "@prisma/client";
-import { FriendsVisibility, ProfileVisibility } from "@prisma/client";
+import { VisibilityStatus } from "@prisma/client";
 
 interface SettingsProps {
-  friendsVisible: FriendsVisibility;
-  profileVisible: ProfileVisibility;
+  friendsVisible: VisibilityStatus;
+  profileVisible: VisibilityStatus;
   userId: string;
 }
 
