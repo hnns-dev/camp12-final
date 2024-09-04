4ba07146589437537e2f37eeb6fb785d2b89ea15
HaillSeitan <150841231+HaillSeitan@users.noreply.github.com>
2024-09-04 11:21:23 +0200

    added email input to form

--- app/profile/[userId]/ProfileForm.tsx
+++ app/profile/[userId]/ProfileForm.tsx
@@ -7,68 +7,45 @@ function handleSubmit() {}
 
 export function ProfileForm({ user }: { user: User }) {
   const [name, setName] = useState(user.name ?? "");
+  const [email, setEmail] = useState(user.email ?? "");
   const [friendsVisibility, setFriendsVisibility] = useState("");
   const [profileVisibility, setProfileVisibility] = useState("");
   return (
-    <>
-      <p>Test </p>
-      <form onSubmit={handleSubmit} className="w-full max-w-sm">
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="name"
-          >
-            Name
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
+    <form onSubmit={handleSubmit} className="w-full max-w-sm p-4">
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="email"
+        >
+          E-Mail
+        </label>
+        <input
+          id="email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </div>
 
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="friendsVisibility"
-          >
-            Friends Visibility
-          </label>
-          <select
-            id="friendsVisibility"
-            value={friendsVisibility}
-            onChange={(e) => setFriendsVisibility(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          >
-            <option value="Private">Private</option>
-            <option value="Public">Public</option>
-          </select>
-        </div>
-
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="profileVisibility"
-          >
-            Profile Visibility
-          </label>
-          <select
-            id="profileVisibility"
-            value={profileVisibility}
-            onChange={(e) => setProfileVisibility(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          >
-            <option value="Private">Private</option>
-            <option value="Public">Public</option>
-          </select>
-        </div>
-
-        <Button type="submit" variant="default">
-          Speichern
-        </Button>
-      </form>
-    </>
+      <Button type="submit" variant="default">
+        Speichern
+      </Button>
+    </form>
   );
 }
