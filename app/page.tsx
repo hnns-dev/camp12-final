aebdcd4d73140d5b325246ea68958da27a7613cf
HaillSeitan <150841231+HaillSeitan@users.noreply.github.com>
2024-09-05 14:13:02 +0200

    add magic link mail template, add versions for new and already existing users

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Navbar from "../components/Navbar";
 import Search from "@/components/Search";
 import Filter from "@/components/Filter";
 import MapAndDrawer from "@/components/MapAndDrawer";
+import { MagicLinkTemplate } from "@/emails/magic-link";
+import { url } from "inspector";
 
 export default function Home() {
   return (
