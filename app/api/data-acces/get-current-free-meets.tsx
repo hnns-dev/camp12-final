ce2387400f44638665904dd3156b1474531cad26
Fred <frederik.koenig@posteo.de>
2024-09-09 09:09:48 +0200

    safety commit

--- app/api/data-acces/get-current-free-meets.tsx
+++ app/api/data-acces/get-current-free-meets.tsx
@@ -1,12 +1,14 @@
 import { prisma } from "@/lib/db";
 
 export async function getCurrentFreeMeets() {
-const currentFreeMeets = await prisma.meet.findMany({
-  where: {
-    meet.venue: null,
-    time
-  },
-});
-return currentFreeMeets;
-
+  const currentFreeMeets = await prisma.meet.findMany({
+    where: {
+      // location: {
+      //   isNot: null,
+      // },
+      venueId: null,
+      Venue: null,
+    },
+  });
+  return currentFreeMeets;
 }
