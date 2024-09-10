4a5a151c07967509ede34cc7a3d4af0d9d49ca7b
ShaQay3Q <shaghay3gh@gmail.com>
2024-09-10 15:46:59 +0200

    add buttins

--- components/DrawerCreatVenue.tsx
+++ components/DrawerCreatVenue.tsx
@@ -12,13 +12,26 @@ import {
 	DrawerDescription,
 } from "./ui/drawer";
 
+import { Button, buttonVariants } from "./ui/button";
+
 export function DrawerCreatVenue() {
 	return (
 		<Drawer>
 			<DrawerTrigger>
 				<FaCirclePlus className='size-8 fill-white' />{" "}
 			</DrawerTrigger>
-			<DrawerContent className='z-[9999] h-[calc(40vh-20vh)] flex flex-col pb-0'></DrawerContent>
+			<DrawerContent className='z-[9999] h-[calc(40vh-20vh)] w-full flex flex-col gap-3'>
+				<DrawerHeader>
+					<DrawerTitle className='sr-only'>Create venue</DrawerTitle>
+					<DrawerDescription className='sr-only'>
+						Create a new activity
+					</DrawerDescription>
+				</DrawerHeader>
+				<div className='flex flex-row w-full items-center justify-between px-6 '>
+					<Button className='h-11 w-32 items-center py-6'>Create Meet</Button>
+					<Button className='h-11 w-32 items-center py-6'>Add Venue</Button>
+				</div>
+			</DrawerContent>
 		</Drawer>
 	);
 }
