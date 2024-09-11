4e33ec65c9b91b5d7c25cd57907f90ede57d5ce9
Fred <frederik.koenig@posteo.de>
2024-09-11 11:00:51 +0200

    deleted hardcoded profile page, updated link from navbar to real profile

--- app/badges/[userId]/page.tsx
+++ app/badges/[userId]/page.tsx
@@ -2,15 +2,32 @@ import React from "react";
 import Image from "next/image";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import { prisma } from "@/lib/db";
 
-export default function BadgesComponent() {
+export default async function BadgesComponent({
+  params,
+}: {
+  params: {
+    userId: string;
+  };
+}) {
   // Example badge data - replace with your actual data
-  const badges = [
-    { id: 1, src: "/profileImg.png", alt: "Badge 1" },
-    { id: 2, src: "/profileImg.png", alt: "Badge 2" },
-    { id: 3, src: "/profileImg.png", alt: "Badge 3" },
-    { id: 4, src: "/profileImg.png", alt: "Badge 4" },
-  ];
+
+  const badges = await prisma.badge.findMany({
+    where: {
+      users: {
+        some: {
+          id: params.userId,
+        },
+      },
+    },
+  });
+  // const badges = [
+  //   { id: 1, src: "/profileImg.png", alt: "Badge 1" },
+  //   { id: 2, src: "/profileImg.png", alt: "Badge 2" },
+  //   { id: 3, src: "/profileImg.png", alt: "Badge 3" },
+  //   { id: 4, src: "/profileImg.png", alt: "Badge 4" },
+  // ];
 
   const selectedBadge = {
     id: 1,
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,22 +20,19 @@ export default function Navbar({
 }: Props) {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-	return (
-		<nav className='flex rounded-3xl absolute z-[999] bottom-4 right-4 left-4 p-5 bg-zinc-800/80 justify-between items-center'>
-			<DrawerUpComingSessions />
-			<FaLocationCrosshairs className='size-8 fill-white' />
-			<DrawerCreatActivity />
-			<Link
-				href='/profile'
-				className='nav-link'
-			>
-				<div className='nav-button'>
-					<div className='flex flex-col items-center justify-center'>
-						<FaUser className='size-8 fill-white' />
-					</div>
-					{/* Profile Content*/}
-				</div>
-			</Link>
-		</nav>
-	);
+  return (
+    <nav className="flex rounded-3xl absolute z-[999] bottom-4 right-4 left-4 p-5 bg-zinc-800/80 justify-between items-center">
+      <DrawerUpComingSessions />
+      <FaLocationCrosshairs className="size-8 fill-white" />
+      <DrawerCreatActivity />
+      <Link href="/profile/me" className="nav-link">
+        <div className="nav-button">
+          <div className="flex flex-col items-center justify-center">
+            <FaUser className="size-8 fill-white" />
+          </div>
+          {/* Profile Content*/}
+        </div>
+      </Link>
+    </nav>
+  );
 }
