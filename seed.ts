d04709bb20fb4bd6561068c24de546675b7b2367
Hydrateurself <bensn@posteo.de>
2024-09-11 10:29:20 +0200

    show/hide profile information based on settings plus city model

--- seed.ts
+++ seed.ts
@@ -1,18 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { connect } from "http2";
 
 const prisma = new PrismaClient();
 
 async function main() {
-	// Fetch venue IDs dynamically
+  // Fetch venue IDs dynamically
 
-	console.log("Cleaning Database...");
+  console.log("Cleaning Database...");
 
-	await prisma.user.deleteMany();
-	await prisma.activityType.deleteMany();
-	await prisma.tag.deleteMany();
-	await prisma.badge.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.activityType.deleteMany();
+  await prisma.tag.deleteMany();
+  await prisma.badge.deleteMany();
+  await prisma.city.deleteMany();
 
-	console.log("Cleaning Database finished");
+  console.log("Cleaning Database finished");
 
   // Activity Types
   const basketball = await prisma.activityType.create({
@@ -98,48 +100,54 @@ async function main() {
     },
   });
 
-	// Users
-	const user1 = await prisma.user.create({
-		data: {
-			id: "aserifkt547eu392",
-			email: "user1@example.com",
-			name: "Hans Meiser",
-			settings: {
-				create: {
-					friendsVisibility: "Private",
-					profileVisibility: "Private",
-				},
-			},
-		},
-	});
+  // Users
+  const user1 = await prisma.user.create({
+    data: {
+      id: "aserifkt547eu392",
+      email: "user1@example.com",
+      name: "Hans Meiser",
+      city: {
+        create: {
+          name: "Leipzig",
+        },
+      },
+      settings: {
+        create: {
+          friendsVisibility: "Private",
+          profileVisibility: "Private",
+        },
+      },
+    },
+  });
 
-	const user2 = await prisma.user.create({
-		data: {
-			id: "aserifkt547eu323",
-			email: "user2@example.com",
-			name: "Tine Wittler",
-			settings: {
-				create: {
-					friendsVisibility: "FriendsOnly",
-					profileVisibility: "FriendsOnly",
-				},
-			},
-		},
-	});
+  const user2 = await prisma.user.create({
+    data: {
+      id: "aserifkt547eu323",
+      email: "user2@example.com",
+      name: "Tine Wittler",
+      settings: {
+        create: {
+          friendsVisibility: "FriendsOnly",
+          profileVisibility: "FriendsOnly",
+        },
+      },
+    },
+  });
 
-	const user3 = await prisma.user.create({
-		data: {
-			id: "as222fkt547eu392",
-			email: "user3@example.com",
-			name: "Conchita Wurst",
-			settings: {
-				create: {
-					friendsVisibility: "Public",
-					profileVisibility: "Public",
-				},
-			},
-		},
-	});
+  const user3 = await prisma.user.create({
+    data: {
+      id: "as222fkt547eu392",
+      email: "user3@example.com",
+      name: "Conchita Wurst",
+      cityName: "Berlin",
+      settings: {
+        create: {
+          friendsVisibility: "Public",
+          profileVisibility: "Public",
+        },
+      },
+    },
+  });
 
   // Tags
   await prisma.tag.create({ data: { name: "Outdoor" } });
@@ -290,181 +298,181 @@ async function main() {
     },
   });
 
-	// Meets (including one tournament)
-	await prisma.meet.create({
-		data: {
-			date: new Date("2024-09-15"),
-			time: "14:00",
-			duration: 2,
-			isPublic: false,
-			creatorId: user1.id,
-			participants: { connect: [{ id: user2.id }, { id: user3.id }] },
-			guests: 2,
-			notes: "Freundliches Basketballspiel",
-			tags: { connect: [{ name: "Outdoor" }] },
-			venueId: weisseElster.id,
-			activityTypeId: basketball.id,
-			isCompetitive: false,
-			isRecurring: false,
-		},
-	});
+  // Meets (including one tournament)
+  await prisma.meet.create({
+    data: {
+      date: new Date("2024-09-15"),
+      time: "14:00",
+      duration: 2,
+      isPublic: false,
+      creatorId: user1.id,
+      participants: { connect: [{ id: user2.id }, { id: user3.id }] },
+      guests: 2,
+      notes: "Freundliches Basketballspiel",
+      tags: { connect: [{ name: "Outdoor" }] },
+      venueId: weisseElster.id,
+      activityTypeId: basketball.id,
+      isCompetitive: false,
+      isRecurring: false,
+    },
+  });
 
-	await prisma.meet.create({
-		data: {
-			date: new Date("2024-09-20"),
-			time: "10:00",
-			duration: 1,
-			isPublic: true,
-			creatorId: user2.id,
-			participants: { connect: [{ id: user1.id }] },
-			guests: 1,
-			notes: "Tennistraining",
-			tags: { connect: [{ name: "Outdoor" }] },
-			venueId: beachClubCossi.id,
-			activityTypeId: tennis.id,
-			isCompetitive: false,
-			isRecurring: false,
-		},
-	});
+  await prisma.meet.create({
+    data: {
+      date: new Date("2024-09-20"),
+      time: "10:00",
+      duration: 1,
+      isPublic: true,
+      creatorId: user2.id,
+      participants: { connect: [{ id: user1.id }] },
+      guests: 1,
+      notes: "Tennistraining",
+      tags: { connect: [{ name: "Outdoor" }] },
+      venueId: beachClubCossi.id,
+      activityTypeId: tennis.id,
+      isCompetitive: false,
+      isRecurring: false,
+    },
+  });
 
-	await prisma.meet.create({
-		data: {
-			date: new Date("2024-09-18"),
-			time: "11:00",
-			duration: 3,
-			isPublic: false,
-			creatorId: user3.id,
-			participants: { connect: [{ id: user1.id }, { id: user2.id }] },
-			guests: 2,
-			notes: "Tennistraining",
-			tags: { connect: [{ name: "Outdoor" }] },
-			venueId: beachClubCossi.id,
-			activityTypeId: tennis.id,
-			isCompetitive: false,
-			isRecurring: false,
-		},
-	});
-	await prisma.meet.create({
-		data: {
-			date: new Date("2024-09-10"),
-			time: "16:00",
-			duration: 2,
-			isPublic: true,
-			creatorId: user1.id,
-			participants: { connect: [{ id: user2.id }, { id: user3.id }] },
-			guests: 2,
-			notes: "Freundliches Basketballspiel",
-			tags: { connect: [{ name: "Outdoor" }] },
-			venueId: weisseElster.id,
-			activityTypeId: basketball.id,
-			isCompetitive: false,
-			isRecurring: false,
-		},
-	});
+  await prisma.meet.create({
+    data: {
+      date: new Date("2024-09-18"),
+      time: "11:00",
+      duration: 3,
+      isPublic: false,
+      creatorId: user3.id,
+      participants: { connect: [{ id: user1.id }, { id: user2.id }] },
+      guests: 2,
+      notes: "Tennistraining",
+      tags: { connect: [{ name: "Outdoor" }] },
+      venueId: beachClubCossi.id,
+      activityTypeId: tennis.id,
+      isCompetitive: false,
+      isRecurring: false,
+    },
+  });
+  await prisma.meet.create({
+    data: {
+      date: new Date("2024-09-10"),
+      time: "16:00",
+      duration: 2,
+      isPublic: true,
+      creatorId: user1.id,
+      participants: { connect: [{ id: user2.id }, { id: user3.id }] },
+      guests: 2,
+      notes: "Freundliches Basketballspiel",
+      tags: { connect: [{ name: "Outdoor" }] },
+      venueId: weisseElster.id,
+      activityTypeId: basketball.id,
+      isCompetitive: false,
+      isRecurring: false,
+    },
+  });
 
-	await prisma.meet.create({
-		data: {
-			date: new Date("2024-09-12"),
-			time: "14:00",
-			duration: 2,
-			isPublic: false,
-			creatorId: user2.id,
-			participants: { connect: [{ id: user3.id }] },
-			guests: 2,
-			notes: "Freundliches Basketballspiel",
-			tags: { connect: [{ name: "Outdoor" }] },
-			venueId: musselGym.id,
-			activityTypeId: tennis.id,
-			isCompetitive: false,
-			isRecurring: false,
-		},
-	});
+  await prisma.meet.create({
+    data: {
+      date: new Date("2024-09-12"),
+      time: "14:00",
+      duration: 2,
+      isPublic: false,
+      creatorId: user2.id,
+      participants: { connect: [{ id: user3.id }] },
+      guests: 2,
+      notes: "Freundliches Basketballspiel",
+      tags: { connect: [{ name: "Outdoor" }] },
+      venueId: musselGym.id,
+      activityTypeId: tennis.id,
+      isCompetitive: false,
+      isRecurring: false,
+    },
+  });
 
-	await prisma.tournament.create({
-		data: {
-			date: new Date("2024-10-01"),
-			time: "09:00",
-			duration: 6,
-			size: 16,
-			type: "Knockout",
-			mode: "Singles",
-			public: true,
-			creatorId: user1.id,
-			participants: { connect: [{ id: user3.id }, { id: user2.id }] },
-			guests: ["Barbara Schöneberger", "Günther Jauch"],
-			note: "Jährliches Tennisturnier",
-			tags: { connect: [{ name: "Outdoor" }, { name: "Indoor" }] },
-			venueId: musselGym.id,
-			activityTypeId: tennis.id,
-		},
-	});
+  await prisma.tournament.create({
+    data: {
+      date: new Date("2024-10-01"),
+      time: "09:00",
+      duration: 6,
+      size: 16,
+      type: "Knockout",
+      mode: "Singles",
+      public: true,
+      creatorId: user1.id,
+      participants: { connect: [{ id: user3.id }, { id: user2.id }] },
+      guests: ["Barbara Schöneberger", "Günther Jauch"],
+      note: "Jährliches Tennisturnier",
+      tags: { connect: [{ name: "Outdoor" }, { name: "Indoor" }] },
+      venueId: musselGym.id,
+      activityTypeId: tennis.id,
+    },
+  });
 
-	// Badges
-	await prisma.badge.create({
-		data: {
-			name: "Anzeigenhauptmeister",
-			icon: "/parkverbot.png",
-			users: {
-				connect: {
-					id: user2.id,
-				},
-			},
-		},
-	});
+  // Badges
+  await prisma.badge.create({
+    data: {
+      name: "Anzeigenhauptmeister",
+      icon: "/parkverbot.png",
+      users: {
+        connect: {
+          id: user2.id,
+        },
+      },
+    },
+  });
 
-	await prisma.badge.create({
-		data: {
-			name: "Turniersieger",
-			icon: "/gold.svg",
-		},
-	});
+  await prisma.badge.create({
+    data: {
+      name: "Turniersieger",
+      icon: "/gold.svg",
+    },
+  });
 
-	await prisma.badge.create({
-		data: {
-			name: "Yoga-Meister",
-			icon: "/yoga.svg",
-		},
-	});
+  await prisma.badge.create({
+    data: {
+      name: "Yoga-Meister",
+      icon: "/yoga.svg",
+    },
+  });
 
-	// Reports
-	await prisma.report.create({
-		data: {
-			issue: "Defekte Ausrüstung",
-			date: new Date("2024-08-10"),
-			time: "16:30",
-			detail: "Der Basketballkorb ist beschädigt und muss repariert werden",
-			venueId: weisseElster.id,
-		},
-	});
+  // Reports
+  await prisma.report.create({
+    data: {
+      issue: "Defekte Ausrüstung",
+      date: new Date("2024-08-10"),
+      time: "16:30",
+      detail: "Der Basketballkorb ist beschädigt und muss repariert werden",
+      venueId: weisseElster.id,
+    },
+  });
 
-	await prisma.report.create({
-		data: {
-			issue: "Hygieneproblem",
-			date: new Date("2024-08-15"),
-			time: "09:00",
-			detail: "Die Umkleidekabinen müssen gründlich gereinigt werden",
-			venueId: musselGym.id,
-		},
-	});
+  await prisma.report.create({
+    data: {
+      issue: "Hygieneproblem",
+      date: new Date("2024-08-15"),
+      time: "09:00",
+      detail: "Die Umkleidekabinen müssen gründlich gereinigt werden",
+      venueId: musselGym.id,
+    },
+  });
 
-	await prisma.report.create({
-		data: {
-			issue: "unschön",
-			date: new Date("2024-08-20"),
-			time: "11:00",
-			detail: "Ist üble Yuppiescheisse hier",
-			venueId: beachClubCossi.id,
-		},
-	});
+  await prisma.report.create({
+    data: {
+      issue: "unschön",
+      date: new Date("2024-08-20"),
+      time: "11:00",
+      detail: "Ist üble Yuppiescheisse hier",
+      venueId: beachClubCossi.id,
+    },
+  });
 
-	console.log("Seed-Daten erfolgreich eingefügt");
+  console.log("Seed-Daten erfolgreich eingefügt");
 }
 
 await main()
-	.catch((e) => {
-		console.error(e);
-	})
-	.finally(async () => {
-		await prisma.$disconnect();
-		process.exit(0);
-	});
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  });
