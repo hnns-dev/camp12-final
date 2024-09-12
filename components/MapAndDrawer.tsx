4653c0e075822db510f0aa09a5cf4d33ef9a3161
Fred <frederik.koenig@posteo.de>
2024-09-12 10:36:13 +0200

    fix homepage

--- components/MapAndDrawer.tsx
+++ components/MapAndDrawer.tsx
@@ -5,13 +5,19 @@ import { DrawerHompage } from "./DrawerHomepage";
 import { GetVenuesResult } from "@/app/api/data-acces/get-venues";
 import { GetOpenMeetsResult } from "@/app/api/data-acces/get-open-meets";
 import { VenueData } from "./Map"; // Import VenueData type
+import Navbar from "./Navbar";
+import { UserCreatedMeet, UserParticipatingMeet } from "@/lib/utils/getMeets";
 
 export default function MapAndDrawer({
   venues,
   openMeets,
+  userCreatedMeets,
+  userPariticpatingMeets,
 }: {
   venues: GetVenuesResult;
   openMeets: GetOpenMeetsResult;
+  userCreatedMeets: UserCreatedMeet[];
+  userPariticpatingMeets: UserParticipatingMeet[];
 }) {
   const Map = useMemo(
     () =>
@@ -22,6 +28,7 @@ export default function MapAndDrawer({
     []
   );
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isUpcomingDrawerOpen, setIsUpcomingDrawerOpen] = useState(false);
   const [selectedVenue, setSelectedVenue] = useState<VenueData | null>(null);
 
   const openDrawer = (venueData: VenueData) => {
@@ -41,6 +48,11 @@ export default function MapAndDrawer({
         isOpen={isDrawerOpen}
         setIsOpen={setIsDrawerOpen}
         venueData={selectedVenue}
+      />
+      <Navbar
+        userCreatedMeets={userCreatedMeets}
+        userPariticpatingMeets={userPariticpatingMeets}
+        isDrawerOpen={isUpcomingDrawerOpen}
       />
     </div>
   );
