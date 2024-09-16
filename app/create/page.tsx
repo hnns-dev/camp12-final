d32e31c467e95b9498cf9bcde3d3f6e347d8b672
Fred <frederik.koenig@posteo.de>
2024-09-16 12:23:02 +0200

    made it get activities from the db, added link to create activity

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -10,30 +10,39 @@ export default async function CreateMeet({
 
   const user = await protectPage();
 
+  const activityTypes = await prisma.activityType.findMany();
+
   if (searchParams.location) {
     // Location was given in url query
     const locationArray = JSON.parse(searchParams.location);
     return (
       <div>
-        <MeetForm userId={user.id} location={locationArray} />
+        <MeetForm
+          userId={user.id}
+          location={locationArray}
+          activityTypes={activityTypes}
+        />
       </div>
     );
   } else if (searchParams.venueId) {
     // Called with venueId
-   const venue = await prisma.venue.findUnique({
-    where: {
-      id: searchParams.venueId
-      }
+    const venue = await prisma.venue.findUnique({
+      where: {
+        id: searchParams.venueId,
+      },
     });
-   if (venue) {
-    return (
-      <div>
-        <MeetForm userId={user.id} venueId={venue.id} venueName={venue.name} />
-      </div>
-    );
-   }
-
-    
+    if (venue) {
+      return (
+        <div>
+          <MeetForm
+            userId={user.id}
+            venueId={venue.id}
+            venueName={venue.name}
+            activityTypes={activityTypes}
+          />
+        </div>
+      );
+    }
   }
   
 
