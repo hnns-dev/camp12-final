5d0305a575ae050fa8cff7627fe6f3a9d519c545
Dan McAtee <danielmcatee@me.com>
2024-09-18 16:06:23 +0200

    fix submitMeet import

--- app/api/new-venue/route.ts
+++ app/api/new-venue/route.ts
@@ -1,24 +1,22 @@
 "use server";
 
 import { NextResponse, NextRequest } from "next/server";
-import { submitMeet } from "../../../actions/meet";
-
+import { submitMeetWithVenue } from "@/actions/meet";
 
 export async function POST(request: NextRequest) {
+  try {
+    const url = new URL(request.url);
+    const userId = url.searchParams.get("userId") as string;
+    const venueId = url.searchParams.get("venueId") as string;
+    const body = await request.json();
 
-    try {
-        const url = new URL(request.url);
-        const userId = url.searchParams.get("userId") as string;
-        const venueId = url.searchParams.get("venueId") as string;
-        const body = await request.json();
+    const meet = await submitMeetWithVenue(body, userId, venueId);
 
-        const meet = await submitMeet(body, userId, venueId);
-  
-        return NextResponse.json(meet);
-    } catch (error) {
-      return NextResponse.json(
-        { error: "Interner Serverfehler" },
-        { status: 500 }
-      );
-    }
+    return NextResponse.json(meet);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Interner Serverfehler" },
+      { status: 500 }
+    );
   }
+}
